f9832a3806db5a4a24e7c52da3580449980843a1
g-larose <larose.g@fromtier.com>
2023-12-03 19:19:52 -0500

    house keeping

--- Football_Pool_Tracker/App.xaml.cs
+++ Football_Pool_Tracker/App.xaml.cs
@@ -1,14 +1,38 @@
-﻿using System.Configuration;
+﻿using Football_Pool_Tracker.UI.MVVM.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace Football_Pool_Tracker
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder().ConfigureServices(services =>
+            {
+                services.AddSingleton<MainWindow>(s => new MainWindow()
+                {
+                    DataContext = new AppViewModel()
+                });
+            }).Build();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+        }
     }
 
 }
