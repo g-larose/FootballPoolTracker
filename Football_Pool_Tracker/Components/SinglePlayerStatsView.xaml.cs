f43ee451ffe42bb3534c0f84e77a0e2928b5de14
g-larose <larose.g@fromtier.com>
2023-12-12 03:57:31 -0500

    added SinglePlayerStatView

---
+++ Football_Pool_Tracker/Components/SinglePlayerStatsView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Football_Pool_Tracker.UI.Components;
+
+public partial class SinglePlayerStatsView : UserControl
+{
+    public SinglePlayerStatsView()
+    {
+        InitializeComponent();
+    }
+}
